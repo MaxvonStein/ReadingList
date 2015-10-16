a2e1e49961a36c278061fb57f7a771831a0e359d
MaxvonStein <mevonstein@gmail.com>
2015-10-16 15:41:22 -0400

    (search form with title only)

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,9 @@ class Book < ActiveRecord::Base
 
   scope :finished, lambda { where.not(finished_on: nil) }
   scope :recent, lambda { where('finished_on < ?', 2.days.ago) }
+  scope :search, ->(keyword){ where(title: keyword) if keyword.present? }
+
+
 
   def finished?
     finished_on.present?
