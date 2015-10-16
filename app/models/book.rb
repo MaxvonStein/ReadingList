d91e4cf95f2a4a8fdb4a982e26e5f01a7d0c4112
MaxvonStein <mevonstein@gmail.com>
2015-10-16 14:14:19 -0400

    custom finished and recent scopes added to book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ActiveRecord::Base
+
+  scope :finished, lambda { where.not(finished_on: nil) }
+  scope :recent, lambda { where('finished_on < ?', 2.days.ago) }
+
   def finished?
     finished_on.present?
   end
