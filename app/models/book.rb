e601b998fee9c58342383f5c568ca71f848c5eaf
MaxvonStein <mevonstein@gmail.com>
2015-10-20 10:19:25 -0400

    added genres and genre search, and made it more efficient

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,15 @@
 class Book < ActiveRecord::Base
 
+  has_many :book_genres
+  has_many :genres, through: :book_genres
+
   scope :finished, lambda { where.not(finished_on: nil) }
   scope :recent, lambda { where('finished_on < ?', 2.days.ago) }
   scope :search, ->(keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
+  # scope :filter, ->(genre_id){ joins(:book_genres).where(book_genres: { genre_id: genre_id }) if genre_id.present? }
+  scope :filter, ->(name){
+    joins(:genres).where('genres.name = ?', name) if name.present?
+  }
 
   before_save :set_keywords
 
